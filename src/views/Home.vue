2339dfd266bcfcfceab57dc46b900b077524cb85
changxianglin <281532612@qq.com>
2023-06-17 16:16:30 +0800

    first components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  home
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
+import { defineComponent } from 'vue';
+export default defineComponent({
   name: 'Home',
+  props: {
+
+  }, 
   components: {
-    HelloWorld
+
+  },
+
+  setup(props, ctx) {
+    return {
+
+    }
   }
-}
+})
 </script>
+
+<style scoped lang="scss">
+
+</style>

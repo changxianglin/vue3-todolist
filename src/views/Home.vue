a65cc8b3c5fda675cd47699cfe4e03921530c45b
changxianglin <281532612@qq.com>
2023-06-26 19:57:12 +0800

    use vuex useStore and computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,21 @@
     <nav-header></nav-header>
     <nav-main></nav-main>
     <nav-footer></nav-footer>
+    <!-- {{ num1 }} --- {{ num2 }}
+    两个数的和 {{ addNum }}
+    <div>
+      <button @click="add">add</button>
+    </div> -->
+    {{ list }}
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, reactive, toRefs } from 'vue';
+import { defineComponent, ref, reactive, toRefs, computed } from 'vue';
 import NavHeader from '../components/navHeader/NavHeader.vue'
 import NavMain from '../components/navMain/NavMain.vue'
 import NavFooter from '../components/navFooter/NavFooter.vue'
+import { useStore } from 'vuex';
 
 export default defineComponent({
   name: 'Home',
@@ -24,6 +31,10 @@ export default defineComponent({
   },
 
   setup(props, ctx) {
+    let store = useStore()
+    let list = computed(() => {
+      return store.state.list
+    })
     // let num = ref(10)
     // let name = ref('zhangsan')
     // let arr = ref(['a', 'b', 'c', 'd'])
@@ -36,12 +47,28 @@ export default defineComponent({
       }
     })
 
+    let num1 = ref(10)
+    let num2 = ref(20)
+    let addNum = computed(() => {
+      return num1.value + num2.value
+    })
+
+    let add = () => {
+      num1.value++
+      num2.value++
+    }
+
     return {
       // num, 
       // name,
       // arr
       // data
-      ...toRefs(data)
+      ...toRefs(data),
+      num1,
+      num2,
+      addNum,
+      add,
+      list, 
     }
   }
 })

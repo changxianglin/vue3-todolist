aef169f554c5503935617fef30a26dcf5c3d957d
changxianglin <281532612@qq.com>
2023-06-26 19:44:09 +0800

    use vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    list: [
+      {
+        title: '吃饭',
+        complete: false,
+      },
+      {
+        title: '睡觉',
+        complete: false,
+      },
+      {
+        title: '打代码',
+        complete: true,
+      }
+    ],
   },
   mutations: {
+    addTodos(state, payload) {
+      state.list.push(payload)
+    },
+    delTodo(state, payload) {
+      state.list.splice(payload, 1)
+    },
+    clearn(state, payload) {
+      state.list = payload
+    },
   },
   actions: {
   },

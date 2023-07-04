1afeba2db4ccf2e57390aeb9e0113b85a1aa21f5
changxianglin <281532612@qq.com>
2023-07-05 01:29:09 +0800

    add parent and child

---
+++ src/components/child/child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>This is an child page</h1>
+    parant transform data is {{ msg }}
+    <button @click="send">transform data to parant</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'Child',
+  props: {
+    msg: {
+      type: String,
+      required: false,
+      default: '默认值',
+    }
+  },
+  setup(props, ctx) {
+    console.log('props', props)
+    let childMsg = ref('这是子组件的数据')
+
+    const send = () => {
+      ctx.emit('send', childMsg.value)
+    }
+
+    return {
+      childMsg,
+      send,
+    }
+  }
+})
+</script>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an detail page</h1>
+    <child :msg="msg" @send="send" />
   </div>
 </template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import child from '../components/child/child.vue'
+export default defineComponent({
+  name: 'Detail',
+  components: {
+    child,
+  },
+  setup() {
+    let msg = ref('这是父组件的数据')
+    let send = (val) => {
+      console.log(val)
+    }
+
+    return {
+      msg,
+      send,
+    }
+  }
+})
+</script>
